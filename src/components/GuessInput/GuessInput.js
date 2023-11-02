3961bed3e149138c5aa6b2b2eef61c094927b127
Scott Lougheed <scott@scottlougheed.com>
2023-11-02 06:41:58 -0700

    Exercise 1 complete

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from 'react';
 
 function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+  }
+
   return (
     <div>
-      <form className="guess-input-wrapper">
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
         <label htmlFor="guessInput">Enter guess:</label>
-        <input type="text" id="guessInput" name="guessInput"></input>
+        <input
+          type="text"
+          id="guessInput"
+          name="guessInput"
+          pattern="^[a-zA-Z]{5,5}$"
+          required
+          value={guess}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        ></input>
       </form>
     </div>
   );
