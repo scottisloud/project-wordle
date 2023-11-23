6b270100d8974d1971305039e8d58effd47d5854
Scott Lougheed <scott@scottlougheed.com>
2023-11-23 06:36:00 -0800

    renders guesses in grid
    does not render empty grid

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+
+function Guess({ submitttedGuess }) {
+  const guess = range(5).map((letter) => (
+    <span className="cell">{submitttedGuess[letter]}</span>
+  ));
+  return <>{guess}</>;
+}
+
+export default Guess;
+
+// function Grid({ numRows, numCols }) {
+//   const rows = range(numRows).map(row => (
+//     <div className="row" key={row}>
+//       {range(numCols).map(col =>(
+//         <div className="cell" key={col}></div>
+//       ))}
+//     </div>
+//   ))
+
+//   return (
+//     <div className="grid">
+//       {
+//         rows.map(row =>(
+//           row
+//         ))
+//       }
+//     </div>
+//   );
+// }
--- src/components/GuessList/GuessResults.js
+++ src/components/GuessList/GuessResults.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
 
+import { range } from '../../utils';
 function GuessList({ guesses }) {
+  const rows = range(6).map((row) =>
+    // stuff here
+    console.log(row)
+  );
   return (
     <div className="guess-results">
       {guesses.map((guess) => (
         <p className="guess" key={Math.random()}>
-          {guess}
+          <Guess submitttedGuess={guess} />
         </p>
       ))}
     </div>
