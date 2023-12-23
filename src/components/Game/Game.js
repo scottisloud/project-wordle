84b96d8b9a3373172f4bc72628ee49bf5c3480ef
Scott Lougheed <scott@scottlougheed.com>
2023-12-23 08:41:24 -0800

    implementing game logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -21,7 +21,7 @@ function Game() {
 
   return (
     <>
-      <GuessResults guesses={guesses} />
+      <GuessResults guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const checkedGuess = checkGuess(value, answer);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
+        <span
+          key={num}
+          className={`cell ${checkedGuess ? checkedGuess[num].status : ''}`}
+        >
+          {checkedGuess ? checkedGuess[num].letter : undefined}
         </span>
       ))}
     </p>
